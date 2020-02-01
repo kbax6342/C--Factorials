1a14f3045eb3921d78a069264b71dd72a87538d0
Kevin Baxter <baxterhome@BaxterDevPro.local>
2020-02-01 02:20:16 -0500

    first commit/working app

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Factorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int total = 1;
+            Console.WriteLine("Please enter a number to factorial:");
+            int factorial = int.Parse(Console.ReadLine());
+
+            for(int i = factorial; i >= 1; i--){
+                
+                    Console.Write($"{i}\tx\t");
+                     total *= i;
+                
+
+              
+            }
+            
+            Console.WriteLine($" {total}");
+            
+                    }
+    }
+}
